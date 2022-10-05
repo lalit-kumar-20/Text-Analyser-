3b0cb041501a362a0fd730f7549907589a2ae61b
LALIT KUMAR <92217028+lalit-kumar-20@users.noreply.github.com>
2022-10-05 23:08:01 +0530

    Add files via upload

---
+++ build/precache-manifest.bfd01cbc0b18887f77e51312a371f61d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0cfa521e157597485b7",
+    "url": "/lalit-kumar-20/Text-Analyser/static/css/main.13748b48.chunk.css"
+  },
+  {
+    "revision": "d0cfa521e157597485b7",
+    "url": "/lalit-kumar-20/Text-Analyser/static/js/main.68223eb3.chunk.js"
+  },
+  {
+    "revision": "025bb9a06e54def463b6",
+    "url": "/lalit-kumar-20/Text-Analyser/static/js/runtime~main.af087da3.js"
+  },
+  {
+    "revision": "37ba4a728a48a6dd8b35",
+    "url": "/lalit-kumar-20/Text-Analyser/static/js/2.f8d8463d.chunk.js"
+  },
+  {
+    "revision": "70380ff939c998672254",
+    "url": "/lalit-kumar-20/Text-Analyser/static/js/3.b7dfee96.chunk.js"
+  },
+  {
+    "revision": "3d804accdd938642ae324e25b0b213e0",
+    "url": "/lalit-kumar-20/Text-Analyser/index.html"
+  }
+];
